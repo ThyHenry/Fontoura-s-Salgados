c7d5e868d6f79d2619ea91ea1e277231f5add720
Thierry <thierryhenry130406@gmail.com>
2024-03-02 17:35:11 -0300

    content

---
+++ json.js
@@ -0,0 +1,149 @@
+let Products= [{
+    id: 1,
+    food_name: "30 salgados mistos",
+    description: "descrição",
+    price: "25,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.40.jpeg",
+    option: "",
+},
+
+{
+    id: 2,
+    food_name: "Casalzinho",
+    description: "descrição",
+    price: "30,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.41 (1).jpeg",
+    option: "",
+},
+
+{
+    id: 3,
+    food_name: "75 salgados mistos",
+    description: "descrição",
+    price: "65,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.41 (2).jpeg",
+    option: "",
+},
+
+
+{
+    id: 4,
+    food_name: "50 salgados mistos",
+    description: "descrição",
+    price: "40,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.41 (3).jpeg",
+    option: "",
+},
+
+
+{
+    id: 5,
+    food_name: "50 salgados mistos + 10 pasteis doces",
+    description: "descrição",
+    price: "50,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.41.jpeg",
+    option: "",
+},
+
+
+{
+    id: 6,
+    food_name: "Na casa da sogra",
+    description: "descrição",
+    price: "75,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.42 (1).jpeg",
+    option: "",
+},
+
+
+{
+    id: 7,
+    food_name: "20 salgados mistos",
+    description: "descrição",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.42.jpeg",
+    option: "",
+},
+
+
+{
+    id: 8,
+    food_name: "Só pra mim",
+    description: "descrição",
+    price: "23,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.43 (1).jpeg",
+    option: "",
+},
+
+
+// pizza brotinho
+{
+    id: 9,
+    food_name: "Familia 1",
+    description: "descrição",
+    price: "90,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.43.jpeg",
+    option: "",
+},
+
+
+{
+    id: 10,
+    food_name: "100 salgados mistos",
+    description: "descrição",
+    price: "80,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.44 (1).jpeg",
+    option: "",
+},
+
+
+{
+    id: 11,
+    food_name: "20 pasteis de chocolate preto",
+    description: "",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.38.44.jpeg",
+    option: "",
+},
+
+
+{
+    id: 12,
+    food_name: "20 pasteis carne moida",
+    description: "",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.41.22.jpeg",
+    option: "",
+},
+
+
+{
+    id: 13,
+    food_name: "20 bolinhas de queijo",
+    description: "",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.41.23 (1).jpeg",
+    option: "",
+},
+
+
+{
+    id: 14,
+    food_name: "20 enroladinhos de salsicha",
+    description: "",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.41.23 (2).jpeg",
+    option: "",
+},
+
+// Salgados mistos
+{
+    id: 15,
+    food_name: "20 coxinhas de frango",
+    description: "",
+    price: "20,00",
+    image: "../img/Image/WhatsApp Image 2024-02-25 at 12.41.23.jpeg",
+    option: "",
+},
+
+]
--- scrip.js
+++ scrip.js
@@ -1,5 +1,7 @@
 const show = document.getElementById("show");
 const nav = document.getElementById("nav_hide");
+const header = document.getElementById("header");
+const main = document.getElementById("main");
 
 show.addEventListener("click", ()=>{
     if(show.getAttribute("src") == "img/icons8-menu-arredondado-50.png") {
@@ -15,4 +17,17 @@ show.addEventListener("click", ()=>{
     } else {
         nav.style.display = "none";
     }
+})
+
+
+Products.map((item, index)=>{
+
+    let product = document.querySelector("#clone .conteiner").cloneNode(true);
+
+    document.getElementById("layout").append(product);
+
+    product.querySelector(".center .responsive_food").innerHTML = item.food_name;
+   product.querySelector(".center .ingredients").innerHTML = item.description;
+   product.querySelector(".center .responsive_price").innerHTML = "R$ " + item.price;
+   product.querySelector(".product .food").src = item.image;
 })
